0254d8ec62c245c63e2d0620881d6e31d91329b2
gideonagbavor8 <gagbavor@byupathway.edu>
2025-02-04 02:35:26 +0000

    Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -27,7 +27,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the Order_Details table
